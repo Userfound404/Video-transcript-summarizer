43464de5059d30f17d025cee1eb78a40ec43347a
Adithya Penagonda <97509220+Userfound404@users.noreply.github.com>
2022-12-31 12:32:12 +0530

    old version, without slider

--- app.py
+++ app.py
@@ -7,36 +7,20 @@ from youtube_transcript_api import YouTubeTranscriptApi
 
 # Create a function to summarize the transcript
 def summarize_transcript(transcript, max_length=512):
+    transcript_text = ' '.join([t['text'] for t in transcript])
     summarizer = pipeline("summarization")
-    transcript_text = [t['text'] for t in transcript]
-    # Divide the transcript into shorter chunks
-    chunk_size = max_length - 2  # Leave some space for the summarizer to add punctuation
-    chunks = [transcript_text[i:i + chunk_size] for i in range(0, len(transcript_text), chunk_size)]
-
-    # Summarize each chunk separately
-    summaries = []
-    for chunk in chunks:
-        summary = summarizer(chunk)[0]["summary_text"]
-        summaries.append(summary)
-
-    # Join the summaries back together
-    summary = ' '.join(summaries)
-
+    summary = summarizer(transcript_text)[0]["summary_text"]
     return summary
 
-
-
+# Create the main Streamlit app
 def main():
-    
     st.title("Video Transcript Summarizer")
-    video_url = st.text_input("Enter the URL of the video:")
-    try:
-        video_id = video_url.split("=")[1]
-    except IndexError:
-        st.write("Error: Invalid video URL")
-        return
 
+    # Retrieve the transcript for the video
+    video_url = st.text_input("Enter the URL of the video:")
+    video_id = video_url.split("=")[1]
     try:
+        # video_id = video_url.split("=")[1]
         YouTubeTranscriptApi.get_transcript(video_id)
         transcript = YouTubeTranscriptApi.get_transcript(video_id)
     except requests.exceptions.RequestException as e:
@@ -46,16 +30,13 @@ def main():
     # Display the video preview and transcript
     st.markdown("**Video Preview:**")
     st.video(video_url)
-    transcript_text = ' '.join([t['text'] for t in transcript])
     st.markdown("**Transcript:**")
+    transcript_text = ' '.join([t['text'] for t in transcript])
     st.text_area("transcript", transcript_text, height=400)
-
-    # Create a slider widget for customizing the size of the summary
-    max_length = st.slider("Summary size:", min_value=256, max_value=1024, value=512, step=256)
 
     # Display the summary of the transcript
     st.markdown("**Summary:**")
-    st.text_area("summary", summarize_transcript(transcript, max_length=max_length))
+    st.text_area("summary", summarize_transcript(transcript))
 
 if __name__ == "__main__":
     main()
