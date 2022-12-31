64614091d070e11760fa6a2535ecb7a74ebc5dd7
Adithya Penagonda <97509220+Userfound404@users.noreply.github.com>
2022-12-31 12:12:39 +0530

    added slider

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from youtube_transcript_api import YouTubeTranscriptApi
 # Create a function to summarize the transcript
 def summarize_transcript(transcript, max_length=512):
     transcript_text = ' '.join([t['text'] for t in transcript])
-    summarizer = pipeline("summarization")
+    summarizer = pipeline("summarization",max_length=max_length)
     summary = summarizer(transcript_text)[0]["summary_text"]
     return summary
 
@@ -18,9 +18,13 @@ def main():
 
     # Retrieve the transcript for the video
     video_url = st.text_input("Enter the URL of the video:")
-    video_id = video_url.split("=")[1]
     try:
-        # video_id = video_url.split("=")[1]
+        video_id = video_url.split("=")[1]
+    except IndexError:
+        st.write("Error: Invalid video URL")
+        return
+
+    try:
         YouTubeTranscriptApi.get_transcript(video_id)
         transcript = YouTubeTranscriptApi.get_transcript(video_id)
     except requests.exceptions.RequestException as e:
@@ -30,12 +34,13 @@ def main():
     # Display the video preview and transcript
     st.markdown("**Video Preview:**")
     st.video(video_url)
+    transcript_text = ' '.join([t['text'] for t in transcript])
     st.markdown("**Transcript:**")
-    st.text_area("transcript", transcript, height=400)
+    st.text_area("transcript", transcript_text, height=400)
+
+    # Create a slider widget for customizing the size of the summary
+    max_length = st.slider("Summary size:", min_value=256, max_value=1024, value=512, step=256)
 
     # Display the summary of the transcript
     st.markdown("**Summary:**")
-    st.text_area("summary", summarize_transcript(transcript))
-
-if __name__ == "__main__":
-    main()
+    st.text_area("summary", summarize_transcript(transcript, max_length=max_length))
