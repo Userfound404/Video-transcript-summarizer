0fdc891e7d22ba7b621fa10c21c2de6d2078d8fe
Adithya Penagonda <97509220+Userfound404@users.noreply.github.com>
2022-12-31 12:08:40 +0530

    list formatting

--- app.py
+++ app.py
@@ -7,8 +7,9 @@ from youtube_transcript_api import YouTubeTranscriptApi
 
 # Create a function to summarize the transcript
 def summarize_transcript(transcript, max_length=512):
+    transcript_text = ' '.join([t['text'] for t in transcript])
     summarizer = pipeline("summarization")
-    summary = summarizer(transcript)[0]["summary_text"]
+    summary = summarizer(transcript_text)[0]["summary_text"]
     return summary
 
 # Create the main Streamlit app
