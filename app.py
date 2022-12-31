440e049a93877519c471dbf6adb9b9eede0e18c9
Adithya Penagonda <97509220+Userfound404@users.noreply.github.com>
2022-12-31 12:56:42 +0530

    buffer length added

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ from transformers import pipeline
 import tensorflow as tf
 from youtube_transcript_api import YouTubeTranscriptApi
 
-
 # Create a function to summarize the transcript
-def summarize_transcript(transcript, max_length=512):
+def summarize_transcript(transcript, buffer_length=50):
     summarizer = pipeline("summarization")
     transcript_text = ' '.join([t['text'] for t in transcript])
+    max_length = len(transcript) + buffer_length
     # Divide the transcript into shorter chunks
     chunk_size = max_length - 2  # Leave some space for the summarizer to add punctuation
     chunks = [transcript_text[i:i + chunk_size] for i in range(0, len(transcript_text), chunk_size)]
